1f00cd86d33da7c1e9ee0d87e6c688a053523d45
Hamza Irfan <Hamza1AtWork@gmail.com>
2024-08-09 02:43:12 +0500

    before doing tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,8 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App-Stack';
-
+import App from '../App';
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
 
